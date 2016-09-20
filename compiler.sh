ca2b0adc54dc197f32965390934e42dea04e5f26
Maarten Dungen <maarten.dungen@gmail.com>
2016-09-20 15:58:22 +0200

    renamed comptest.sh to compiler.sh in root dir

---
+++ compiler.sh
@@ -0,0 +1,10 @@
+
+path=./src/chrgen/
+
+def=build/test
+outdir=${1:-$def}
+
+mkdir -p $outdir
+node $path/compileFiles.js -o $outdir testchr/compilerPanel.js
+node $path/compileFiles.js -o $outdir testchr/playground.js
+node $path/compileFiles.js -o $outdir testchr/testelem.js
